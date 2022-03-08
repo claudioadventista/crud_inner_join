8a24eb9b942e335fd22b16ebe4c1b90a2c685e5b
José Cláudio Xavier <claudioadventista@hotmail.com>
2022-03-08 17:23:16 -0300

    atualizando index

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once 'conexao.php';
-
+// PREENCHE AS INFORMAÇÕES DA TABELA USANDO INNER JOIN NA CONSULTA
    $query = mysqli_query(
    $conexao,"SELECT 
    cadastro.id, 
@@ -44,7 +44,7 @@ require_once 'conexao.php';
             
             <!-- primeiro formulário de pesquisa --> 
             <form id="formulario" name="frmEnviaDados" class="form-horizontal" action="busca.php"  method="post">
-                <input id="cpf" type="text" name="busca" placeholder="Digite aqui para novo cadastro" autocomplete="off" onkeyup="verificar()">
+                <input id="cpf" type="text" name="busca" placeholder="Digite aqui para Novo Cadastro" autocomplete="off" onkeyup="verificar()">
                 <input  id="verifica" class="botao cadastro" type="submit" value="verificar" onclick="return buscar()">
                 <a href="duplicado.php" class="botao but-azul" > página cliente </a>
                 
@@ -52,8 +52,8 @@ require_once 'conexao.php';
         
              <span class="texto">Digite para buscar o cadastro</span>
             <form id="buscaCadastro" name="frmEnviaDados" class="form-horizontal" action="busca.php"  method="post">
-                <input id="buscaCad" type="text" name="buscaCad" placeholder="Digite o id ou codigo de barra" autocomplete="off" />
-                <input  id="buscar" class="botao but-azul" type="submit" value="buscar" onclick="buscaNoBanco3();  return false;"> 
+                <input id="buscaCad" type="text" name="buscaCad" placeholder="Digite a O.S. ou Código de Barras" autocomplete="off" />
+                <input  id="buscar" class="botao but-azul" type="submit" value="buscar" onclick="desabilitaCampos(); buscaNoBanco3();  return false;"> 
             </form>
         </div> 
 
@@ -143,7 +143,7 @@ require_once 'conexao.php';
                             <td><?php echo $categoria['marca_nome']; ?></td>
                             <td><?php echo $categoria['estado_nome']; ?></td>
                             <td >
-                                <span class="botao but-azul" onclick="buscaCad.value='<?php echo $categoria['id']; ?>'; desabilitaCampos(); buscaNoBanco3()" >alt</span>
+                                <span class="botao but-azul" onclick="buscaCad.value='<?php echo $categoria['cadastro_barra']; ?>'; desabilitaCampos(); buscaNoBanco3()" >alt</span>
                                 <a href="excluir_cadastro.php?codigo=<?= $categoria['id']; ?>" class="botao but-vermelho"  OnClick="return confirm('Confirma Exclusão da OS <?php echo $categoria['cadastro_ordem']; ?>' +'\n'+'<?php echo $categoria['cliente_nome']; ?>')" >exc</a>
                             </td>
                         </tr>
@@ -153,11 +153,12 @@ require_once 'conexao.php';
         </div>            
     </div>
     <?php
+        // CARREGA AS FUNÇÕES JAVASCRIPT PARA ESSA PÁGINA
         require_once 'js.php';
     ?>
     <script>
 
-        // Valida o cpf digitado na entrada
+        // VALIDA O CPF DIGITADO NO CAMPO DE ENTRADA
         function verificar(){
             let cpf =  document.getElementById('cpf');
             // conta o número de caracteres até chegar o valor para fazer a validação
@@ -187,7 +188,7 @@ require_once 'conexao.php';
             }
         }; 
 
-        // Chama essa função quando clica no botão VERIFICAR
+        // CHAMA ESSA FUNÇÃO QUANDO CLICA NO BOTÃO "VERIFICAR"
         function buscar(){
             // retira os espaços da string com a função trim(), e conta os caracteres que sobrarem
             var buscar =  document.getElementById('cpf').value.trim();
@@ -201,11 +202,10 @@ require_once 'conexao.php';
                 return false;
         };
 
-        // Campo - Digite o nome, cpf, telefone, email ou codigo de barra
-        // verifica no banco, se o cpf, ou o nome, ou o código de barras digitado 
-        // na entrada já foi cadastrado.
+        // VERIFICA SE OS DADOS QUE FORAM DIGITADO NO CAMPO DE ENTRADA, ESTÃO CADASTRADOS NO BANCO
         function buscaNoBanco(){
             let texto = document.getElementById('cpf').value;
+
             fetch('http://localhost:80/inner_join_mysql_tres_tabelas/buscaCadastro.php?verifica=' + texto)
             .then(response => {// retorna a requisição fetch
                 if (response.ok) {// se reornar ok
@@ -257,71 +257,129 @@ require_once 'conexao.php';
             });   
         };
 
-            // Valida o cpf digitado no formulário 
-            function verificar2(){
-                let cpf2 =  document.getElementById('cpf2');  
-                if(cpf2.value.length >0){
-                    submitCadastro.disabled ="true";
-                    submitCadastro.style.background ="#f1f1f1";
-                    submitCadastro.style.color ="#aaa";
-                    submitCadastro.style.borderColor ="#aaa";
-                    submitCadastro.style.cursor ="default";
+        // VALIDA O CPF DIGITADO NO FORMULÁRIO 
+        function verificar2(){
+            let cpf2 =  document.getElementById('cpf2');  
+            if(cpf2.value.length >0){
+                submitCadastro.disabled ="true";
+                submitCadastro.style.background ="#f1f1f1";
+                submitCadastro.style.color ="#aaa";
+                submitCadastro.style.borderColor ="#aaa";
+                submitCadastro.style.cursor ="default";
 
-                    if(cpf2.value.length == 11){
-                        // verifica se todos os caracers do campo são números
-                        if (!isNaN(cpf2.value)){
-                            // faz a validação
-                            var resultado2 = isValidCPF(cpf2.value);
-                            // mostra o resultado
-                            if(resultado2 ===true){
-                                buscaNoBanco2();
-                            }else{
-                                cpf2.style.background = "#f00";
-                                cpf2.style.color ="#fff";           
-                            } 
-                        }
-                    }else{
-                        cpf2.style.background = "#fff";
-                        cpf2.style.color ="#000";
-                    };
+                if(cpf2.value.length == 11){
+                    // Verifica se todos os caracers do campo são números
+                    if (!isNaN(cpf2.value)){
+                        // Faz a validação
+                        var resultado2 = isValidCPF(cpf2.value);
+                        // Mostra o resultado
+                        if(resultado2 ===true){
+                            buscaNoBanco2();
+                        }else{
+                            cpf2.style.background = "#f00";
+                            cpf2.style.color ="#fff";           
+                        } 
+                    }
+                }else{
+                    cpf2.style.background = "#fff";
+                    cpf2.style.color ="#000";
+                };
+            }else{
+                submitCadastro.disabled ="";
+                submitCadastro.style.background ="lightgreen";
+                submitCadastro.style.color ="#000";
+                submitCadastro.style.borderColor ="#999";
+                submitCadastro.style.cursor ="pointer";
+            }
+        };
+
+        // VERIFICA SE O CPF DIGITADO NO FORMULÁRIO JÁ ESTÁ CADASTRADO NO BANCO
+        function buscaNoBanco2(){
+            let texto2 = document.getElementById('cpf2').value;
+            
+            fetch('http://localhost:80/inner_join_mysql_tres_tabelas/buscaCadastro.php?buscaCPF=' + texto2)
+            .then(response => {// retorna a requisição fetch
+                if (response.ok) {// se reornar ok
+                return response.json();// converte num objeto json
+                }
+            })
+
+            .then(json => {
+                if(json > 0){
+                    alert("CPF já cadastrado");
                 }else{
                     submitCadastro.disabled ="";
                     submitCadastro.style.background ="lightgreen";
                     submitCadastro.style.color ="#000";
                     submitCadastro.style.borderColor ="#999";
-                    submitCadastro.style.cursor ="pointer";
-                }
-            };
+                    submitCadastro.style.cursor ="pointer"; 
+                };
+            })
 
-             
-            // Verifica no banco, se o cpf válido digitado no formuário já esta cadastrado
-            function buscaNoBanco2(){
-                let texto2 = document.getElementById('cpf2').value;
-                fetch('http://localhost:80/inner_join_mysql_tres_tabelas/buscaCadastro.php?buscaCPF=' + texto2)
-                .then(response => {// retorna a requisição fetch
-                    if (response.ok) {// se reornar ok
-                    return response.json();// converte num objeto json
-                    }
-                })
+            .catch(error => {
+                alet("Erro ao consultar o cpf no banco");
+            }); 
+        };
 
-                .then(json => {
-                    if(json.cpf == texto2){
-                        alert("CPF já cadastrado");
-                    };
-                    if(json == "zero"){
-                        submitCadastro.disabled ="";
-                        submitCadastro.style.background ="lightgreen";
-                        submitCadastro.style.color ="#000";
-                        submitCadastro.style.borderColor ="#999";
-                        submitCadastro.style.cursor ="pointer"; 
-                    };
-                })
+       
 
-                .catch(error => {
-                    alet("Erro ao consultar o cpf no banco");
-                }); 
-            };// fim da função buscar banco
+        function desabilitaCampos(){
+        ordemServico.disabled = 'true';
+        nome.disabled = 'true';
+        dtNascimento.disabled = 'true';
+        dtCadCliente.disabled = 'true';
+        dtEntrada.disabled = 'true';
+       // defeito.disabled = 'true';
+        acessorio.disabled = 'true';
+        select_aparelho.disabled = 'true';
+        select_marca.disabled = 'true';
+        select_defeito.disabled = 'true';
+        novoAparelho.disabled = 'true';
+        novaMarca.disabled = 'true';
+        novoDefeito.disabled = 'true';
+        document.getElementById("form-cadastro").innerHTML="alterar cadastro";
+    };
+    function desabilitaExistente(){
+        if(cpf2.value!=""){
+            //cpf2.disabled = 'true';
+        };
+        cpf.disabled = 'true';
+        nome.disabled = 'true';
+        dtNascimento.disabled = 'true';
+        dtCadCliente.disabled = 'true';
+    };
 
+    function reabilitaCampos(){
+        ordemServico.disabled = '';
+        //cpf2.disabled = '';
+        cpf.disabled = '';
+        cpf.value='';
+        cpf.style.background = "#fff";
+        cpf.style.color = "#000";
+        cpf2.style.background = "#fff";
+        cpf2.style.color = "#000";
+        nome.disabled = '';
+        dtNascimento.disabled = '';
+        dtCadCliente.disabled = '';
+        dtEntrada.disabled = '';
+      //  defeito.disabled = '';
+        acessorio.disabled = '';
+        buscaCad.value='';
+        id_aparelho.value='';
+        id_aparelho.text='';
+        id_marca.value='';
+        id_marca.text='';
+        id_estado.value='';
+        id_estado.text='';
+        id_defeito.value='';
+        id_defeito.text='';
+        select_aparelho.disabled = '';
+        select_marca.disabled = '';
+        select_defeito.disabled = '';
+        novoAparelho.disabled = '';
+        novaMarca.disabled = '';
+        novoDefeito.disabled = '';
+    };
     </script>
 </body>
 </html>
